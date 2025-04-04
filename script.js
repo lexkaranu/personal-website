4e8d88e294d2808bd993bbee6202c579d473cef0
lexkaranu <160360798+lexkaranu@users.noreply.github.com>
2025-04-05 01:11:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+const greetingElement = document.getElementById("greeting");
+const hours = new Date().getHours();
+if (hours < 12) {
+    greetingElement.innerText = "Good Morning!";
+} else if (hours < 18) {
+    greetingElement.innerText = "Good Afternoon!";
+} else {
+    greetingElement.innerText = "Good Evening!";
+}
+
